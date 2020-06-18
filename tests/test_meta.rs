ab8f54c1afb53c5785eb3e3c9450f89fbf1de9ad
Jim Fasarakis-Hilliard <d.f.hilliard@gmail.com>
2020-06-18 22:39:31 +0300

    Initial implementation for proc macros. (#1)
    We've started by parsing the items passed in and then passing these
    along to the implementation functions. The tests all pass even
    after applying to macros to each item (and doing a very silly
    transformation).

--- tests/test_meta.rs
+++ tests/test_meta.rs
@@ -1,11 +1,12 @@
 extern crate suptest;
 use suptest::{unit, integration};
 
-// actually no issue with this, apparently.
-#[test]
-#[test]
-#[integration]
-#[unit]
+#[integration("test")]
 fn test_test(){
     assert_eq!(1, 1);
+}
+
+#[unit("test")]
+fn test_two_test(){
+    assert_eq!(2, 2)
 }
