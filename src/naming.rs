a76d3a3f046bc5f154874e92095250e64063428b
Jim Fasarakis-Hilliard <d.f.hilliard@gmail.com>
2020-06-20 18:25:18 +0300

    A working but not final solution. (#3)
    Add an initial working solution. impl_rename in naming.rs
    calls rename_fn for applications on a single function and
    handle_mod for applications on a whole module.
    
    These could be generalized, I would believe.

--- src/naming.rs
+++ src/naming.rs
@@ -1,17 +1,25 @@
 extern crate proc_macro;
 extern crate alloc;
 use alloc::string::ToString;
-use proc_macro::TokenStream;
-use quote::quote;
-use syn;
+use proc_macro::{TokenStream};
+use quote::{quote, format_ident};
+use syn::{
+    parse, parse::{Parser}, parse_macro_input,
+    AttributeArgs, Attribute, Signature,
+    Item, ItemFn, ItemMod
+};
 
+
+// For my own ease.
 type TS = TokenStream;
 
 // Handle cases - Unit and Integration.
+#[derive(Clone, Copy)]
 pub(crate) enum NameType{
     Unit,
     Integration,
 }
+
 
 impl ToString for NameType {
     fn to_string(&self) -> String {
@@ -22,29 +30,93 @@ impl ToString for NameType {
     }
 }
 
-pub(crate) fn impl_rename(attrs: TS, f: TS, mode: NameType) -> TS {
-    // Eventually might need to support fn and mod
-    let function: syn::ItemFn = syn::parse(f)
-        .expect("Unable to parse function.");
 
-    // may very well be empty
-    let attributes = attrs.clone();
-    println!("{}", attributes);
-    println!("{}", mode.to_string());
-    /*
-    let attributes: syn::Ident = match syn::parse(attributes){
-        Ok(it) => it,
-        // substitute with empty parents (I'd guess.)
-        Err(_error) => panic!("Temporarily.")
-    };
-    */
-    let fid = quote::format_ident!("{}_{}", function.sig.ident, mode.to_string());
+/// Make the attribute, seems like there should be a cleaner way to do this.
+fn make_attr(name: &str) -> Vec<Attribute> {
+    let id = format_ident!("{}", name);
+    // Make into TokenStream with quote and parse it again.
+    let attr = quote! {
+        #[#id]
+    }.into();
 
-    let gen = quote! {
-        #[test]
-        fn #fid(){
-            assert_eq!(2, 2)
+    Attribute::parse_outer.parse(attr).expect("The unexpected")
+}
+
+/// Apply rename_fn to all the fn's found in the module. Apply #[cfg(test)] to module.
+fn handle_mod(attrs: AttributeArgs, module: ItemMod, mode: NameType) -> TS {
+    // Match for the content (i.e anything defined in module).
+    // empty content => module declaration.
+    let (brace, content) = match module.content {
+        // Grab the items
+        Some(content) => content,
+        None => {
+            panic!("Cannot apply macro to mod declaration.");
         }
     };
-    gen.into()
+    // Go through Items and build result. Non fn's should be ignored.
+    let mut contents: Vec<Item> = Vec::new();
+    for it in content {
+        match it {
+            // Call case to handle fn
+            Item::Fn(it) => {
+                let res = rename_fn(attrs.clone(), it, mode);
+                contents.push(syn::Item::from(res));
+            }
+            // Others, just push them on contents.
+            other => {
+                contents.push(other);
+            }
+        }
+    }
+    // Create new module, add #[cfg(test)] and return.
+    let resmod = ItemMod{
+        content: Some((brace, contents)), ..module
+    };
+    (quote!{
+        #[cfg(test)]
+        #resmod
+    }).into()
+}
+
+
+/// Transform name from fn_name to fn_name_mode.to_string
+fn rename_fn(_attrs: AttributeArgs, function: ItemFn, mode: NameType) -> ItemFn {
+    // Create the new identifier.
+    let ident = format_ident!("{}_{}", function.sig.ident, mode.to_string());
+
+    // add the #[test] attribute and any other attributes added to the function.
+    let mut function_attributes= make_attr("test");
+    function_attributes.extend(function.attrs);
+
+    // create the new signature
+    let sig = Signature{
+        ident, ..function.sig
+    };
+    // and return the new itemfn
+    ItemFn{sig, attrs: function_attributes,  ..function}
+}
+
+
+/// Implement renaming. If a mod, we rename all fn's if fn, simply rename that.
+pub(crate) fn impl_rename(attrs: TS, f: TS, mode: NameType) -> TS {
+    // Grab it as an item.
+    let it: Item = parse(f).expect("Expected an item");
+    let attributes: AttributeArgs = parse_macro_input!(attrs as AttributeArgs);
+    match it {
+        // Handle fn application.
+        Item::Fn(it) => {
+            let res = rename_fn(attributes, it, mode);
+            (quote!{
+                #res
+            }).into()
+        }
+        // Handle module application.
+        Item::Mod(it) => {
+            handle_mod(attributes, it, mode)
+        }
+        // Can't handle this.
+        _ => {
+            panic!("Item must be mod or fn.");
+        }
+    }
 }
--- tests/test_meta.rs
+++ tests/test_meta.rs
@@ -6,8 +6,30 @@ fn test_test(){
     assert_eq!(1, 1);
 }
 
+#[unit]
+fn test_another(){
+    assert_eq!("What is", "What is");
+}
+
+#[unit]
+fn test_another_again(){
+    assert_eq!([1, 2, 3], [1, 2, 3]);
+}
 
 #[integration("", "")]
 fn test_two_test(){
     assert_eq!(2, 2)
+}
+
+#[integration("", "")]
+fn test_another_test(){
+    assert_eq!(vec![2], vec![5-3]);
+}
+
+
+#[unit]
+mod tests{
+    fn test_mod_hi(){
+        assert_eq!("to be", "to be");
+    }
 }
